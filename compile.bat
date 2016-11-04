e88e83b41587bf29f83c3f5092f9a5c92bbba822
Marcel Ruegenberg <m.rue@picodesign.de>
2016-11-04 09:57:37 +0100

    Added VEX shader OTL wrapper for ease of use

--- compile.bat
+++ compile.bat
@@ -1,7 +1,9 @@
-set "HFS=C:\Program Files\Side Effects Software\Houdini 15.5.523"
+set "HOUDINI_DIR=C:\Program Files\Side Effects Software\Houdini 15.5.523"
+set "ARNOLD_DIR=deps/Arnold-4.2.14.4-windows"
 set "PATH=%HFS%\bin;%PATH%"
 
-hcustom.exe -i ../../publish vexrgb.cpp -I eigen -I libigl\include
+"%HOUDINI_DIR%\bin\hcustom.exe" -e -i ./build src/vexrgb.cpp -I %ARNOLD_DIR%/include -L "%ARNOLD_DIR%\lib" -l ai.lib -l CVEX.lib -l boost_system
+rem -l UI.lib -l OPZ.lib -l OP3.lib -l OP2.lib -l OP1.lib -l SIM.lib -l GEO.lib -l PRM.lib -l UT.lib -l boost_system
 rem add -g to compile with debug info
 
 PAUSE
--- src/vexrgb.cpp
+++ src/vexrgb.cpp
@@ -1,5 +1,5 @@
 /*
- * solid texture shader
+ * CVEX shader for Arnold
  */
 
 #include <ai.h>
